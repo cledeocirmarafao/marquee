cb9127d49a4ffe26cce8442399245b5ad2536f58
Cledeocir Marafão <cledeocirms96@gmail.com>
2026-05-07 14:50:15 -0300

    chore: adicionar ajustes, teste de ESLint e remoção de dependências descartadas

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { useState } from "react";
+
 function App() {
-  return <></>;
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <section id="spacer"></section>
+    </>
+  );
 }
 
 export default App;
