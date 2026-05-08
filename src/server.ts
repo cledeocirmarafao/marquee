15c9218974e49c5a11212934d9f9b94759204e5c
Cledeocir Marafão <cledeocirms96@gmail.com>
2026-05-08 10:45:41 -0300

    feat: adicionar rota POST /movies para criação de filmes com prisma e tratamento de erro

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,8 @@ const prisma = new PrismaClient({ adapter });
 const port = 3000;
 const app = express();
 
+app.use(express.json());
+
 app.get("/movies", async (_req, res) => {
   const movies = await prisma.movies.findMany({
     orderBy: {
@@ -20,6 +22,26 @@ app.get("/movies", async (_req, res) => {
     },
   });
   res.json(movies);
+});
+
+app.post("/movies", async (req, res) => {
+  const { title, genre_id, language_id, oscar_count, release_date } = req.body;
+
+  try {
+    await prisma.movies.create({
+      data: {
+        title,
+        genre_id,
+        language_id,
+        oscar_count,
+        release_date: new Date(release_date),
+      },
+    });
+  } catch (err) {
+    res.status(500).json({ error: "Ocorreu um erro ao tentar criar um filme." });
+  }
+
+  res.status(201).send();
 });
 
 app.listen(port, () => {
