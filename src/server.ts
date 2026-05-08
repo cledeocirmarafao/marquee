ee1bcf2947e3d0586346719c4962b77cee18b5dd
Cledeocir Marafão <cledeocirms96@gmail.com>
2026-05-08 09:44:37 -0300

    feat: adicionar rota GET com prisma e express

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,25 @@
 import express from "express";
+import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../generated/prisma/client";
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const prisma = new PrismaClient({ adapter });
 
 const port = 3000;
 const app = express();
 
-app.get("/movies", (req, res) => {
-  res.send("Listagem de filmes");
+app.get("/movies", async (_req, res) => {
+  const movies = await prisma.movies.findMany({
+    orderBy: {
+      title: "asc",
+    },
+    include: {
+      genre: true,
+      language: true,
+    },
+  });
+  res.json(movies);
 });
 
 app.listen(port, () => {
